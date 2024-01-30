3ae163235def597a6bb3a5f4802f18e65f15fe57
Enes Büyük <contact@enesbuyuk.com>
2024-01-30 20:07:52 +0300

    Update squid-game.cpp

--- KODTÜ3/squid-game.cpp
+++ KODTÜ3/squid-game.cpp
@@ -3,32 +3,32 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-bool findTF(int const& x, int const& y, vector<int>& CalculatedFalseValues, int& notFound, int& diff) {
-    CalculatedFalseValues.insert(CalculatedFalseValues.end(), {0,2,1});
+bool findTF(int const& x, int const& y, vector<int>& calculatedFalseValues, int& notFound, int& diff) {
+    calculatedFalseValues.insert(calculatedFalseValues.end(), {0,2,1});
     if (x == 0) return y != 0;
     if (x == 1) return y != 2;
-    if (CalculatedFalseValues[x] != 0) return y != CalculatedFalseValues[x];
+    if (calculatedFalseValues[x] != 0) return y != calculatedFalseValues[x];
     for(int j=notFound;j<=x;j++){
-        if(CalculatedFalseValues[j]!=0) continue;
-        CalculatedFalseValues[j]=j+diff;
-        CalculatedFalseValues[j+diff]=j;
+        if(calculatedFalseValues[j]!=0) continue;
+        calculatedFalseValues[j]=j+diff;
+        calculatedFalseValues[j+diff]=j;
         diff++;
         if((j+diff-1)==x){
-            notFound = (CalculatedFalseValues[j+1]==0) ? j+1:j+2;
+            notFound = (calculatedFalseValues[j+1]==0) ? j+1:j+2;
             return true;
         }
     }
-    notFound = (CalculatedFalseValues[x+1]==0) ? x+1:x+2;
-    return y != CalculatedFalseValues[x];
+    notFound = (calculatedFalseValues[x+1]==0) ? x+1:x+2;
+    return y != calculatedFalseValues[x];
 }
 int main() {
     int numberofMatches, notFound = 3, diff = 2;
-    vector<int> CalculatedFalseValues(850000);
+    vector<int> calculatedFalseValues(850000);
     cin >> numberofMatches;
     while (numberofMatches--) {
         int firstPile, secondPile;
         cin >> firstPile >> secondPile;
-        firstPile<secondPile ? (findTF(firstPile,secondPile,CalculatedFalseValues, notFound, diff) ? cout << "T" << endl : cout << "F" << endl):(findTF(secondPile, firstPile, CalculatedFalseValues, notFound, diff) ? cout << "T" << endl : cout << "F" << endl);
+        firstPile<secondPile ? (findTF(firstPile,secondPile,calculatedFalseValues, notFound, diff) ? cout << "T" << endl : cout << "F" << endl):(findTF(secondPile, firstPile, calculatedFalseValues, notFound, diff) ? cout << "T" << endl : cout << "F" << endl);
     }
     return 0;
 }
